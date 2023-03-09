7984f71e9a1e37d44007a4ca329c5c64a15484a6
byrybdyk <byrybdyk@gmail.com>
2023-03-09 20:31:56 +0300

    new

--- laba5/src/main/java/byrybdyk/me/aplication/Main.java
+++ laba5/src/main/java/byrybdyk/me/aplication/Main.java
@@ -1,5 +1,5 @@
 package byrybdyk.me.aplication;
-//my own branch
+//my own branch byrybdyk
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!!!!");
