daf5ba4b9a8419eea327c8a301344b07328ef318
byrybdyk <byrybdyk@gmail.com>
2023-03-09 20:17:52 +0300

    первый коммит

---
+++ lab5/src/main/java/byrybdyk/me/aplication/Main.java
@@ -0,0 +1,8 @@
+package byrybdyk.me.aplication;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    CollectionManager collectionManager = new CollectionManager();
+}
