5afcb6ae441597eb3f5bc3c22acd22099c1e24a7
byrybdyk <byrybdyk@gmail.com>
2023-03-10 19:01:42 +0300

    add check_command

--- laba5/src/main/java/byrybdyk/me/aplication/CommandReader.java
+++ laba5/src/main/java/byrybdyk/me/aplication/CommandReader.java
@@ -9,6 +9,10 @@ public class CommandReader {
     private CollectionManager collectionManipulator;
 
     private HashMap<String, Command> commands;
+    private HashMap<String, String> double_word_command;
+
+    String user_command;
+    String[] finalUserCommand;
 
     public CommandReader(CollectionManager collectionManager) {
         this.collectionManipulator = collectionManager;
@@ -19,32 +23,47 @@ public class CommandReader {
         commands.put("help", new HelpCommand(commands,collectionManipulator));
         commands.put("info", new InfoCommand(collectionManipulator));
         commands.put("show", new ShowCommand(collectionManipulator));
-        commands.put("insert", new InsertNullCommand(collectionManipulator));
+        commands.put("insert null", new InsertNullCommand(collectionManipulator,user_command));
+
+        double_word_command = new HashMap<>();
+        double_word_command.put("insert", "null");
+
     }
 
-    public HashMap<String, Command> get_commands(){
-        return commands;
-    }
     public void start() {
         Scanner scanner = new Scanner(System.in);
 
 
+
+
         while (true) {
             System.out.print("Enter a command: ");
-            String user_command = scanner.nextLine();
+            user_command = scanner.nextLine();
+            finalUserCommand = user_command.trim().toLowerCase().split(" ", 3);
             Command command = commands.get(user_command);
-            if (commands.containsKey(user_command)){
-                command.execute();
+            if ((double_word_command.containsKey(finalUserCommand[0])) && (finalUserCommand.length >= 2 )) {
+                if (double_word_command.containsKey(finalUserCommand[0]) && (double_word_command.get(finalUserCommand[0]).toString().equals(finalUserCommand[1].toString()))) {
+                    user_command = finalUserCommand[0] + " " + finalUserCommand[1];
+                    command = commands.get(user_command);
+                    command.execute();
+                    continue;
+                }
+                else {
+                    System.out.println("Unknown command");
+                    continue;
+                }
 
             }
-            else {
+
+            if (commands.containsKey(finalUserCommand[0]) ) {
+                command = commands.get(finalUserCommand[0]);
+                command.execute();
+
+            } else {
                 System.out.println("Unknown command");
             }
 
 
         }
-
-
     }
-
 }
--- laba5/src/main/java/byrybdyk/me/commands/InsertNullCommand.java
+++ laba5/src/main/java/byrybdyk/me/commands/InsertNullCommand.java
@@ -7,11 +7,15 @@ import java.util.HashMap;
 public class InsertNullCommand implements Command  {
 
     private CollectionManager collectionManipulator;
+    private String user_command;
 
-    public InsertNullCommand(CollectionManager collectionManipulator) {
+    public InsertNullCommand(CollectionManager collectionManipulator,String user_command) {
         this.collectionManipulator = collectionManipulator;
+        this.user_command = user_command;
     }
 
+
+    long id;
 
     public String getDescription(){
 
